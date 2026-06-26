b3f7502121374f0c808e879454e71da6874c464f
jacobthemanzi-lab <jacobthemanzi@gmail.com>
2026-06-25 21:54:23 -0600

    Add files via upload

---
+++ src/data/songs.js
@@ -0,0 +1,40 @@
+export const songs = [
+  // Stick Season
+  "Stick Season, Stick Season",
+  "Northern Attitude, Stick Season",
+  "Dial Drunk, Stick Season",
+  "Orange Juice, Stick Season",
+  "Everywhere, Everything, Stick Season",
+  "She Calls Me Back, Stick Season",
+  "New Perspective, Stick Season",
+  "Come Over, Stick Season",
+  "Growing Sideways, Stick Season",
+  "Homesick, Stick Season",
+  "All My Love, Stick Season",
+  "Strawberry Wine, Stick Season",
+  "The View Between Villages, Stick Season",
+  "Your Needs, My Needs, Stick Season",
+  "Call Your Mom, Stick Season",
+  "Halloween, Stick Season (Extended)",
+  "Paul Revere, Stick Season (Extended)",
+
+  // Busyhead
+  "False Confidence, Busyhead",
+  "Mess, Busyhead",
+  "Sink, Busyhead",
+  "Cynic, Busyhead",
+  "Young Blood, Busyhead",
+  "Bury Me, Busyhead",
+
+  // Hurt Somebody era / singles
+  "Hurt Somebody, Hurt Somebody",
+  "Passenger, Hurt Somebody",
+  "False Confidence (Acoustic), Singles",
+
+  // Singles / extras
+  "Glue Myself Shut, Singles",
+  "Part of Me, Singles",
+  "Anyway, Singles",
+  "Bad Luck, Singles",
+  "Hold It Down, Singles"
+];
